c2eaeb8c1135f39454fcb1514d594300dc63c834
Daniela Martinez <dany9727@hotmail.com>
2022-10-07 16:21:50 +0200

    Updated yaml file, deleted unnecesary code & files

--- states.py
+++ states.py
@@ -3,11 +3,10 @@ from FeatureCloud.app.engine.app import State as op_state
 from CustomStates import ConfigState
 import subprocess
 import pandas as pd
-import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
 # App name
-name="fc_anonymization"
+name="fc_anonymization_micro"
 
 # FeatureCloud requires that apps define the at least the 'initial' state.
 # This state is executed after the app instance is started.
@@ -15,7 +14,7 @@ name="fc_anonymization"
 class LoadData(ConfigState.State):
 
     def register(self):
-        self.register_transition('WriteAnonymizedData')  # We declare that 'terminal' state is accessible from the 'initial' state.
+        self.register_transition('WriteAnonymizedData')  
 
     def run(self):
         self.lazy_init()
@@ -26,14 +25,13 @@ class LoadData(ConfigState.State):
         output_file= f"{self.output_dir}/{self.config['result']['file']}"
         self.store('output_file', output_file)
         self.store('anonymized_data', df_anom)
-        return 'WriteAnonymizedData'  # This means we are done. If the coordinator transitions into the 'terminal' state, the whole computation will be shut down.
+        return 'WriteAnonymizedData'  
 
     def read_data(self):
         input_files = self.load('input_files')
         data_file = input_files['data'][0]
         format = data_file.split('.')[-1].strip()
         ontologies_folder="/"
-        print(format)
         if format != 'txt':
             self.log(f"The file format {format} is not supported", LogLevel.ERROR)
             self.update(state=op_state.ERROR)
@@ -55,8 +53,6 @@ class LoadData(ConfigState.State):
             if ontology_path != None:
                 tag['ontology']= f"{ontologies_folder}/{ontology_path.split('/')[-1]}"
 
-        print(bs_data.prettify())
-        
         config_file="./config.xml"
 
         with open(config_file, "w", encoding='utf-8') as file:
@@ -64,45 +60,8 @@ class LoadData(ConfigState.State):
         
         return config_file
 
-    def generate_xml(self,ontologies_folder):
-        tree = ET.ElementTree("tree")
-        root = ET.Element("schema")
-        dataset = ET.Element("dataset")
-        for attr in self.config["attributes"]:
-            attribute = ET.SubElement(dataset, "attribute")
-            name = list(attr.keys())[0]
-            attribute.set("name",name)
-            for key, value in attr[name].items():
-                if key == "ontology":
-                    attribute.set("ontology",f".{ontologies_folder}/{value}")
-                else:
-                    attribute.set(key,value)
-        attribute.text= ""
-        root.append(dataset)
-        protection = ET.Element("protection")
-        for attr in self.config["protection"]:
-            attribute = ET.SubElement(protection, "attribute_type")
-            name = list(attr.keys())[0]
-            attribute.set("name",name)
-            for key, value in attr[name].items():
-                attribute.set(key,str(value))
-        attribute.text= ""
-        root.append(protection)
-        tree._setroot(root)
-        config_file="config.xml"
-        tree.write(config_file, encoding = "utf-8", xml_declaration = True, method="html", short_empty_elements=False)
-        with open(config_file, 'r') as f:
-            data = f.read()
-        bs_data = BeautifulSoup(data, 'xml')
-        print(bs_data.prettify())
-        with open(config_file, "w", encoding='utf-8') as file:
-            file.write(bs_data.prettify())
-        return config_file
-
     def anonymized_data(self,data_file,config_file):
-        print(data_file)
-        print(config_file)
-        output = subprocess.getoutput(f"java -jar -Xmx1024m -Xms1024m ./mAnt.jar {data_file} {config_file} ")
+        output = subprocess.getoutput(f"java -jar -Xmx1024m -Xms1024m mAnt1.jar {data_file} {config_file} ")
         print(output)
         file = data_file.split('/')[-1]
         file_name= file.split('.')[0] 
